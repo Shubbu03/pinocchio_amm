9f3e330426a429f654072f75b9514c7d15962af9
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-08-26 22:19:59 +0530

    ixs init

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,12 +1,21 @@
 use pinocchio::program_error::ProgramError;
 
+pub mod deposit;
 pub mod initialize;
+pub mod swap;
+pub mod withdraw;
 
+pub use deposit::*;
 pub use initialize::*;
+pub use swap::*;
+pub use withdraw::*;
 
 #[repr(u8)]
 pub enum ProgramInstruction {
-    InitializeState,
+    Initialize,
+    Deposit,
+    Withdraw,
+    Swap,
 }
 
 impl TryFrom<&u8> for ProgramInstruction {
@@ -14,7 +23,10 @@ impl TryFrom<&u8> for ProgramInstruction {
 
     fn try_from(value: &u8) -> Result<Self, Self::Error> {
         match *value {
-            0 => Ok(ProgramInstruction::InitializeState),
+            0 => Ok(ProgramInstruction::Initialize),
+            1 => Ok(ProgramInstruction::Deposit),
+            2 => Ok(ProgramInstruction::Withdraw),
+            3 => Ok(ProgramInstruction::Swap),
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
